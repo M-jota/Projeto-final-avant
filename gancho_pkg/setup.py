7f5be5ab0fad26431334b8235f5cff1ba4739f46
Letícia Schmidt <leticiast.santos@gmail.com>
2025-11-15 00:25:25 -0300

    add pt2 desafio gancho

---
+++ gancho_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'gancho_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rosuser',
+    maintainer_email='rosuser@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "node_gancho = gancho_pkg.node_gancho_desafio:main"
+        ],
+    },
+)
